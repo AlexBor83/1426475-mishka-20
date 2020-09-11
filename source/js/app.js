aab4b70511c9c1ebb38141c5aa0e087c4b91c397
AlexBor83 <rabota_for_alex@mail.ru>
2020-09-11 23:54:53 +0400

    Исправил ошибки по блоку

--- source/js/app.js
+++ source/js/app.js
@@ -1,9 +1,9 @@
-const mainNav = document.querySelector('.header__mobile'), // находим блок, который у нас будет скрываться/открываться
-  mainNavButton = mainNav.querySelector('.header__toggle'); // находим кнопку открытия/закрытия
+const mainNav = document.querySelector('.header__mobile'),
+  mainNavButton = mainNav.querySelector('.header__toggle');
 
 if (window.innerWidth < 768) {
   mainNav.classList.add('header__mobile--closed');
-  mainNavButton.addEventListener('click', () => { // регистрируем событие клика
+  mainNavButton.addEventListener('click', () => {
     mainNav.classList.toggle('header__mobile--closed');
     mainNavButton.classList.toggle('header__toggle--closed');
     mainNavButton.classList.toggle('header__toggle--opened');
